df599ea5f54f287f2d9b8054ab8b44a628712eb1
mtocav <mtocav@gmail.com>
2025-04-30 14:08:58 +0200

    added auth middleware

---
+++ backend/middleware/userMiddleware.go
@@ -0,0 +1,41 @@
+package middleware
+
+import (
+	"net/http"
+	"os"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v4"
+)
+
+func RequireAuth(c *gin.Context) {
+	// Get the token from the request header
+	tokenString := c.Request.Header.Get("Authorization")
+	if tokenString == "" {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "Authorization header is missing"})
+		c.Abort()
+		return
+	}
+
+	// Remove "Bearer " prefix from the token string
+	tokenString = strings.TrimPrefix(tokenString, "Bearer ")
+
+	// Parse the token
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		// Check if the signing method is HMAC
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, jwt.NewValidationError("unexpected signing method", jwt.ValidationErrorSignatureInvalid)
+		}
+		return []byte(os.Getenv("SECRET")), nil
+	})
+
+	if err != nil || !token.Valid {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid or expired token"})
+		c.Abort()
+		return
+	}
+
+	// Token is valid, proceed to the next handler
+	c.Next()
+}
