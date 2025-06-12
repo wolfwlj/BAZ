39ea44338b3eda023f4cd1c3d5e5bdc86d7b543d
wolfwlj <wolfmbo123@gmail.com>
2025-06-12 13:54:29 +0200

    deleted comment

--- frontend/App.js
+++ frontend/App.js
@@ -5,14 +5,6 @@ import { AuthProvider } from './src/context/AuthContext';
 import AppNavigator from './src/navigation/AppNavigator';
 // import * as SecureStore from 'expo-secure-store';
 
-<<<<<<< HEAD
-=======
-// const token = localStorage.getItem('token');
-// if (token) {
-//   axios.defaults.headers.common['Authorization'] = token;
-// }
-
->>>>>>> f5f2610ba2a6de4fdeef065588c17bba6b05f9b4
 export default function App() {
   return (
     <AuthProvider>
